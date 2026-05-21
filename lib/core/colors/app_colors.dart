b80ce1f60b331c2a72fa0b71e26d6a4344afb215
SalahHassan202 <salahhassan66190@gmail.com>
2026-05-21 19:21:47 +0300

    added more colors

---
+++ lib/core/colors/app_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFFD4845A);
+  static const Color primaryLight = Color(0xFFE8A882);
+  static const Color primarySoft = Color(0xFFF5DDD0);
+
+  static const Color accent = Color(0xFF7EC8C8);
+  static const Color accentSoft = Color(0xFFB8E0E0);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color background = Color(0xFFFAF8F5);
+  static const Color surface = Color(0xFFF5F0EB);
+  static const Color cardBg = Color(0xFFF9F5F1);
+
+  static const Color textPrimary = Color(0xFF2D2320);
+  static const Color textSecondary = Color(0xFF7A6A62);
+  static const Color textMuted = Color(0xFFB0A09A);
+
+  static const Color divider = Color(0xFFE8DDD8);
+  static const Color shadow = Color(0x1A6B4C3B);
+  static const Color overlayDark = Color(0x99000000);
+
+  static const Color cardLavender = Color(0xFFE8D8F0);
+  static const Color cardMint = Color(0xFFD0ECEC);
+  static const Color cardPink = Color(0xFFF5DDD0);
+}
